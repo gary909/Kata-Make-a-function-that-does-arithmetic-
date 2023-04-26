5811b24934cba4b8d7cc1e6548780be733b52270
gary909 <garywhite61@hotmail.com>
2023-04-26 12:54:05 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function arithmetic(a, b, operator){
+    //your code here!
+}
+
+console.log(arithmetic(1, 2, "add")); // 3
+console.log(arithmetic(8, 2, "subtract")); // 6
+console.log(arithmetic(5, 2, "multiply")); // 10
+console.log(arithmetic(8, 2, "divide")); // 4
